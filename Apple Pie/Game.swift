ed3c2bd262dafd8e3d0381ff0bfe993abc9ccf47
robdekker <rob.dekker@student.uva.nl>
2017-11-08 10:42:59 +0100

    Update Game.swift

--- Apple Pie/Game.swift
+++ Apple Pie/Game.swift
@@ -12,12 +12,14 @@ struct Game {
     var word: String
     var incorrectMovesRemaining: Int
     var guessedLetters: [Character]
+    
     mutating func playerGuessed(letter: Character) {
         guessedLetters.append(letter)
         if !word.characters.contains(letter) {
             incorrectMovesRemaining -= 1
         }
     }
+    
     var formattedWord: String {
         var guessedWord = ""
         for letter in word.characters {
