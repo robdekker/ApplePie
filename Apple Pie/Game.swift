a01d730b4c85fd2e4674d5d0ba1c64cae2ac1ba6
Rob Dekker <rob.dekker@student.uva.nl>
2017-11-08 10:25:59 +0100

    Apple Pie First Commit

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,32 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Rob Dekker on 06-11-17.
+//  Copyright © 2017 Rob Dekker. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word.characters {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -10,11 +10,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var listOfWords = ["programmeren", "storyboard", "constraint", "orientatie", "xcode", "swift"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var currentGame: Game!
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+    }
 
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord.characters {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
